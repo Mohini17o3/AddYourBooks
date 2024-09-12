2d9d99346a0897f7d76ec8d8f04b358d2a1ababc
Mohini <upretimohini@gmail.com>
2024-09-13 02:54:21 +0530

    adding some deployment requirements

--- backend/app.py
+++ backend/app.py
@@ -5,20 +5,55 @@ import numpy as np
 from data_processing import get_reading_stats
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 books_read = []
-books_to_read = []  
+books_to_read = [] 
 
 @app.route('/api/add-book' , methods = ['POST'])
 def add_book():
   data = request.json
+  book = {
+      'title' : data['title'],
+      'author' : data['author'],
+      'status' : data['status'],
+      'start_date' : data.get('start_date' , None),
+      'end_date' : data.get('end_date', None),
+      'rating' : data.get('rating' , None),
+      'review' : data.get('review', ''),
+      'date_added' : data.get('date_added' , None),
+  }
   if data['status'] == 'read':
         books_read.append(data)
   else:
         books_to_read.append(data)
 
   return jsonify({'message': 'Book added successfully'}), 200
+
+
+
+
+
+
+@app.route('/api/remove-books' , methods = ['DELETE'])
+def remove_book():
+  data = request.json
+  title = data['title']
+  author = data['author']
+
+
+  for book in books_read:
+      if book['title'] == title and book['author'] == author:
+          books_read.remove(book)
+          return jsonify({"message" : "book removed successfully" , "status" : "read"}) , 200
+      
+  for book in books_to_read:
+      if book['title'] == title and book['author'] == author  : 
+          books_to_read.remove(book)
+          return jsonify({"message" : "book removed successfully ", "status" : "to_read"}) , 200
+          
+ 
+  return jsonify({"message" : "book not found"}) , 404
 
 
 
--- backend/data_processing.py
+++ backend/data_processing.py
@@ -1,21 +1,59 @@
 import pandas as pd
 import numpy as np
 
-
 def get_reading_stats(books_read):
     if not books_read:
-        return{
-            'months':['Jan' , 'Feb' , 'Mar' , 'Apr'] ,
-            'booksRead' : [0 , 0 , 0 , 0]
+        return {
+            'years' : ['2024'],
+            'months': ['Jan', 'Feb', 'Mar', 'Apr'],
+            'booksRead': [2, 4, 1, 0],
+            'average_rating': 3.5,
+            'average_reading_speed': 0.8,
         }
-    
+
     df = pd.DataFrame(books_read)
-    df['data_added'] = pd.to_datetime(df['date_added'])
-    df.set_index('date_added' , inplace=True)
 
-    monthly_counts = df.resample('M').size()
+    df['end_date'] = pd.to_datetime(df['end_date'], errors='coerce')
+    df['start_date'] = pd.to_datetime(df['start_date'], errors='coerce')
 
-    return {
-        'months' : monthly_counts.index.strftime('%b').tolist(),
-        'booksRead':monthly_counts.tolist()
-    }
+    df['rating'] = pd.to_numeric(df['rating'], errors='coerce')  
+    df['rating'] = df['rating'].fillna(0)  
+
+
+    # calculation for days to finish
+    df['days_to_finish'] = (pd.to_datetime(df['end_date']) - pd.to_datetime(df['start_date'])).dt.days
+
+    # unique years from data
+    years = df['end_date'].dt.year.unique().tolist()
+
+    results =  {
+        'years' : [],
+        'months' : [],
+        'booksRead':[] ,
+        'average_rating': [],
+        'average_reading_speed': [] ,
+     }  
+    
+
+
+    for year in years :
+        #filter for current year
+        df_year = df[df['end_date'].dt.year == year]
+         
+        #monthly stats
+        df_year['month'] = df_year['end_date'].dt.strftime('%B')
+        months = pd.date_range(start=f'1/1/{year}' , end = f'12/31/{year}' , freq = 'M').strftime('%B').tolist()
+        books_read_per_month = df_year.groupby('month').size().reindex(months ,fill_value=0).tolist()
+
+        # yearly stats
+        average_rating = df_year['rating'].mean()
+        average_reading_speed = df_year['days_to_finish'].mean()
+
+        results['years'].append(year)
+        results['months'].append(months)
+        results['booksRead'].append(books_read_per_month)
+        results['average_rating'].append(average_rating)
+        results['average_reading_speed'].append(average_reading_speed)
+
+
+    return results
