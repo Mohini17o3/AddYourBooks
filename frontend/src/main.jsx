a3df96537435ce52b5026e84c798a8dbb359ad16
Mohini <upretimohini@gmail.com>
2025-02-20 23:51:33 +0530

    fix : module add

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
       }, 
       {
         path : "/login",
-        element : <Login />
+        element :<Login />
       }, 
       {
         path : "/topBooks",
